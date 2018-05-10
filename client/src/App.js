3933d2eec2fed54abe98636eca06eeb534e34f3b
AngelaF <ADFrys@users.noreply.github.com>
2018-05-10 15:10:40 -0500

    Update Nav bar functionality and add functions to goal form.

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,13 @@ import About from "./pages/About";
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
-import MenuItem from 'material-ui/MenuItem';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import Drawer from 'material-ui/Drawer';
 import Search from './pages/Search';
+import Toggle from 'material-ui/Toggle';
+import { MenuList, MenuItem } from 'material-ui/Menu';
+import Nav from "./components/Nav"
 
 
 function handleClick(e) {
@@ -36,22 +38,21 @@ const buttonStyle = {
 
 const rightButtons = (
   <div>
-    <FlatButton label="Sign Up" style={buttonStyle} to="/">
+    <FlatButton label="Sign Up" style={buttonStyle} to="/Search">
     </FlatButton>
-    <FlatButton label="Log In" style={buttonStyle} to="/" />
+    <FlatButton label="Log In" style={buttonStyle} to="/Userhome" />
   </div>
 );
-
 
 const App = () => (
 
   <Router>
     <div>
       <MuiThemeProvider>
-    
         <AppBar
           title="Progression"
-          onLeftIconButtonClick={handleClick}
+          onTitleClick={handleClick}
+          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
           iconElementRight={rightButtons}
           onRightIconButtonClick={handleClick}
         />
@@ -66,6 +67,7 @@ const App = () => (
       </Switch>
     </div>
   </Router>
+
 );
 
 export default App;
