aebd40438b2bb83668b07ab25580200c2e9ab348
KDarch928 <Kayle.Drucker@gmail.com>
2018-05-10 00:37:38 -0500

    fix upload to s3 bucket

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -29,33 +29,5 @@ export default {
   saveGoal: function(goalData) {
     console.log("API axios.POST(/api/goals")
     return axios.post("/api/goals", goalData);
-  },
-  fileUpload: function(fileData) {
-    
-    axios.get("/api/getdata")
-      .then(data => console.log(data))
-      .catch(err => {
-        if(err.respsone) {
-          console.log(err.respsone.data);
-          console.log(err.respsone.status);
-          console.log(err.respsone.header)
-        } else if (err.request) {
-          console.log(err.request);
-        } else {
-          console.log("Error", err.message);
-        }
-        console.log(err.config);
-      });
-
-    const config = {
-      bucketName: "progressionapp",
-      region: "us-east-2",
-      accessKeyId: "AKIAJWYFYH4AZDO2BKWQ",
-      secretAccessKey: "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"
-    }
-
-    ReactS3.upload(fileData, config)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
   }
 };
