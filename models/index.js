ec31615c1b9ef275360d40e14f7f8d0834d6417b
KDarch928 <Kayle.Drucker@gmail.com>
2018-05-12 18:37:33 -0500

    test

--- models/index.js
+++ models/index.js
@@ -4,13 +4,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-<<<<<<< HEAD
-  Goals: require("./goals"),//,
- User: require("./user")
-};
-=======
   Goals: require("./goals"),
   User: require("./user"),
-  Categories: require("./category")
+  // Categories: require("./category")
 };
->>>>>>> deac64b1f2dae3ab4f44d07815400df9ae1d8fa0
