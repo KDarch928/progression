fa8a7c99f483c88e814de0dcb6cb1de82befe47f
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2018-05-13 13:16:04 -0500

    display latest goals on userhome

--- client/src/pages/Userhome/Userhome.js
+++ client/src/pages/Userhome/Userhome.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../../style.css";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import API from "../../utils/API";
 
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
@@ -9,7 +10,8 @@ import image from '../../images/sun.jpg';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
-
+import {Row,Col,Container} from "../../components/Grid"
+import {List,ListItem} from "../../components/List";
 import RaisedButton from 'material-ui/RaisedButton';
 import Goalform from "../../components/Goalform";
 import Goalheader from "../../components/Goalheader";
@@ -41,10 +43,17 @@ class UserHome extends Component  {
     this.state = {
       expanded: false,
       description: "",
-      category: ""
+      category: "",
+      goals: [],
+      user: "Mary",
+      percent: "",
+      message: "",
     };
   }
 
+componentDidMount() {
+ this.UserGoals()
+}
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
@@ -52,28 +61,156 @@ class UserHome extends Component  {
     });
   };
 
+  UserGoals = () => {
+
+   API.getGoalsUser("Angela")        
+    .then(res => {
+      let tmpgoal = res.data[0].goal;
+      let tmpdata = [];
+      let i = 0
+      while ( i < res.data.length ) {
+
+        tmpgoal = res.data[i].goal
+        //console.log ( "#1 loop i "+i+" goal "+tmpgoal+" percent "+res.data[i].gpercent)
+        
+        if (res.data.length >= 1) 
+        {
+          
+          //console.log("#2 datalength "+res.data.length)
+         
+          if ( i < res.data.length-1
+            && res.data[i+1].goal !== tmpgoal)
+            {
+              if ( i === 0 ) 
+              {
+                tmpdata.push(res.data[0])
+                tmpgoal = res.data[1].goal;
+                console.log("#3 PUSH 0")
+               }
+              i++;
+              continue;         
+            }     
+            else
+              {
+                //console.log("#6 before while no tmpdata equal i "+i+" tmpgoal "+tmpgoal+"percent "+res.data[i].gpercent)
+                //console.log(" length "+res.data.length)
+                while ( i <= res.data.length && res.data[i].goal === tmpgoal ) //res.data[i+1].goal)
+                 {
+                   //console.log("#7 --- while before i inc "+i)
+
+                   tmpgoal = res.data[i].goal
+                   if ( i >=res.data.length-1)
+                     break;
+                   i++
+                 
+                   //if ( i < res.data.length ) 
+                   // {    
+                   //     console.log("#8 --- while after i inc tmpgoal "+tmpgoal+"percent "+res.data[i].gpercent)                 
+                   //     console.log("#9 *** while no tmpdata equal i "+i+" tmpgoal "+tmpgoal+"percent "+res.data[i].gpercent)
+                   // }
+                 } //end while
+
+                 //console.log("#10 %%% --- else after while i "+i +" tmpgoal "+tmpgoal)
+               
+               if ( i < res.data.length-1)
+                i++;
+              } // end else
+            //if ( i < res.data.length)
+            //  console.log("#11 after while goals equal i "+i+" tmpgoal "+tmpgoal+"percent "+res.data[i].gpercent)
+          
+            if ( i < res.data.length-1) 
+            {
+              console.log("#12 PUSH after while push i "+i+" percent "+res.data[i].gpercent)
+              tmpdata.push(res.data[i])
+             
+              if (i < res.data.length-1) 
+              {
+               tmpgoal = res.data[i].goal
+               //console.log("#13 i "+i+" while goal inc bottom percent "+res.data[i].gpercent)
+              }
+            }
+
+        } // end if large
+
+        //console.log("#14 i "+i+" after while before if")
+        if ( i < res.data.length) 
+         {
+            console.log("#15 PUSH after while push i "+i+" "+res.data[i].gpercent+
+            " goal "+res.data[i].goal);
+            tmpdata.push(res.data[i])
+         }
+
+       if (tmpdata.length > 0)
+        {
+          console.log("#16 setState "+JSON.stringify(tmpdata))
+          this.setState({
+           goals: tmpdata
+         })
+        }      
+       i++;
+        //console.log("#17 end of while inc i to "+i)
+      } // end while
+      
+      //console.log("#18 after goals data "+JSON.stringify(this.state.goals))
+
+    } // end .then
+    )
+    .catch(err => console.log(err)) 
+ 
+  }
+
+  handleGoal = (id) => {
+  //let id = '5af4912e04f888219c1c00b1'
+    console.log("handleGoal id "+JSON.stringify(id))
+    // console.log("goals "+this.state.goals)
+    API.getGoal(id)
+    .then (res => {
+        console.log("handleGoal "+JSON.stringify(res.data))
+  
+        //console.log("handleGoal state "+this.state.displayState); //this.displayState())
+        this.setState({
+          goals: res.data,
+          message: !res.data
+            ? "No New Goals Found, Try a Different Goal"
+            : "Here is your goal"
+        })
+      }
+      )
+      .catch(err => console.log(err)) 
+   
+  }
+
   getGoals = () => {
-    axios.get("/api/goals",{
+
+    console.log("getGoals")
+    axios.get("/api/goals/",{
       description: this.state.description,
       category: this.state.category
     })
-      .then(res =>
+      .then(res => {
+         console.log("res "+res.data)
         this.setState({
           description: res.data,
           category: res.data
         })
+
+      }
       )
       .catch(err => console.log(err));
   };
 
   handleFormSubmit = event => {
     event.preventDefault();
-    this.getGoals();
+    console.log("handleFormSubmit")
+    //this.getGoals();
+    this.UserGoals()
   };
 
 
   handleToggle = (event, toggle) => {
     this.setState({expanded: toggle});
+       // this.handleGoal(id)
+//console.log("handleToggle id "+id)
   };
 
   handleExpand = () => {
@@ -84,54 +221,70 @@ class UserHome extends Component  {
     this.setState({expanded: false});
   };
 
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
+
   render() {
 
     return (
       <MuiThemeProvider>
+       <Container>
+       <div style={stylejumbo} className="jumbotron">
+         <h1>Set Your Goals!</h1>
+         <p>Congratulations on taking the first step!</p>
+         <p> You are one step closer to your goal!</p>
 
-      <div style={stylejumbo} className="jumbotron">
-        <h1>Set Your Goals!</h1>
-        <p>Congratulations on taking the first step!</p>
-        <p> You are one step closer to your goal!</p>
-
-        <Goalform 
-        handleInputChange={this.handleInputChange}
-        hangleFormSubmit={this.handleFormSubmit}
-        description={this.state.description}
-        category={this.state.category}
-        />
-
-      </div>
-      <Goalheader />
-      <Card style={color} expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-        <CardHeader
-          title="Goal Name"
-          subtitle="Exercise"
-          avatar={image}
-          actAsExpander={false}
-          showExpandableButton={true}
-        />
-        <CardText>
-          <Toggle
-            toggled={this.state.expanded}
-            onToggle={this.handleToggle}
-            labelPosition="right"
-            label=""
+          <Goalform 
+          handleInputChange={this.handleInputChange}
+          hangleFormSubmit={this.handleFormSubmit}
+          description={this.state.description}
+          category={this.state.category}
           />
-        </CardText>
-        <CardMedia
-          expandable={true}
-          overlay={false}
-        > 
-        </CardMedia>
-        
-        <CardTitle title="Goal Title" subtitle="Fitness" expandable={true} />
-        <CardText expandable={true}>
-          {this.state.description}
-          My goal is to exercise daily
-        </CardText>
-        </Card>
-        <br />
+      </div>
+
+      <Goalheader />
+
+      <div>
+        <List>
+          {this.state.goals.map((goal) => (
+          <div>
+           {/* <p>{goal.gcategory}</p>
+            <p>{goal.gpercent}</p> */}
+            <Card style={color} expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+              <CardHeader
+                title={goal.goal} //"Goal Name"
+                subtitle={goal.category} //"Exercise"
+                avatar={image}
+                actAsExpander={false}
+                showExpandableButton={true}
+              />
+              <CardText>
+                <Toggle
+                  toggled={this.state.expanded}
+                  onToggle={this.handleToggle}
+                  labelPosition="right"
+                  label=""
+                />
+              </CardText>
+              <CardMedia
+                expandable={true}
+                overlay={false}
+              > 
+              </CardMedia> 
+              
+              {/*<CardTitle title="Goal Title" subtitle="Fitness" expandable={true} />*/}        
+              <CardTitle title={goal.goal} subtitle={goal.gcategory} expandable={true} />
+              <CardText expandable={true}>
+                {this.state.description}
+                You are at {goal.gpercent} percent!
+              </CardText>
+            </Card>
+           <br />
+          </div> 
+         ))}
+       </List>
+   </div>
         <Goalsfollowing />
         <div>
           <Card style={color2} expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
@@ -162,6 +315,7 @@ class UserHome extends Component  {
       </Card>
       <br />
       </div>
+     </Container>
     </MuiThemeProvider>
     );
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,57 @@
 // client/src/utils/API.js                M Jordan
 /////////////////////////////////////////////////////////
 import axios from "axios";
-import ReactS3 from "react-s3";
 
 export default {
+  signup: function () {
+    return axios.get('/signup');
+  },
+
+  login: function () {
+    return axios.get('/login');
+  },
+
+  submitSignup: function (userInfo) {
+    // this will respond with error if there was an error
+    // or respond with registered if registered.
+    console.log("submit" + userInfo)
+    return axios.post('/signup', userInfo)
+      .then(function (response) {
+        console.log("api" + response.data);
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("error1")
+      });
+  },
+  submitLogin: function (LoginInfo) {
+    return axios.post('/login', LoginInfo)
+      .then(function (response) {
+        console.log(response);
+        console.log('hitting login');
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log('error at login');
+      });
+  },
+  Logout: function (LoginInfo) {
+    return axios.get('/logout')
+      .then(function (response) {
+        console.log(response);
+        console.log('hitting logout');
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log('error at logout');
+      });
+  },
   // Gets all goals
   getGoals: function(params) {
     console.log("API getGoals axios.GET(/api/goals ")
-      axios.get("/api/goals")
+    axios.get("/api/goals")
       .then(data => {
         console.log(data)
         return data
@@ -28,28 +72,11 @@ export default {
   // Gets the goal with the given id
   getGoal: function(id) {
     console.log("API axios.GET(/api/goal/"+id)
-    axios.get("/api/goals/" + id)
-      .then(data => {
-        console.log(data)
-        return data
-      })
-      .catch(err => {
-        if(err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.header)
-        } else if (err.request) {
-          console.log(err.request);
-        } else {
-          console.log("Error", err.message);
-        }
-      });
-    //return axios.get("/api/goals/" + id);
+    return axios.get("/api/goals/" + id);
   },
-  // Gets the goal with the given id
   getGoalsCategory: function(category) {
     console.log("API axios.GET(/api/goals/category category " + category);
-   axios.get("/api/goals/category/"+category)
+    axios.get("/api/goals/category/"+category)
       .then(data => {
         console.log(data)
         return data
@@ -69,22 +96,7 @@ export default {
     // Gets the goals with the given user
   getGoalsUser: function(user) {
     console.log("API axios.GET(/api/goals/user/" + user);
-    axios.get("/api/goals/user/"+user)
-      .then(data => {
-        console.log(data)
-        return data
-      })
-      .catch(err => {
-        if(err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.header)
-        } else if (err.request) {
-          console.log(err.request);
-        } else {
-          console.log("Error getGoalsUser ", err.message);
-        }
-      });
+     return axios.get("/api/goals/user/" + user);
   },
   // Deletes the goal with the given id
   deleteGoal: function(id) {
@@ -95,33 +107,5 @@ export default {
   saveGoal: function(goalData) {
     console.log("API axios.POST(/api/goals")
     return axios.post("/api/goals", goalData);
-  },
-  fileUpload: function(fileData) {
-    
-    axios.get("/api/getdata")
-      .then(data => console.log(data))
-      .catch(err => {
-        if(err.respsone) {
-          console.log(err.respsone.data);
-          console.log(err.respsone.status);
-          console.log(err.respsone.header)
-        } else if (err.request) {
-          console.log(err.request);
-        } else {
-          console.log("Error", err.message);
-        }
-        console.log(err.config);
-      });
-
-    const config = {
-      bucketName: "progressionapp",
-      region: "us-east-2",
-      accessKeyId: "AKIAJWYFYH4AZDO2BKWQ",
-      secretAccessKey: "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"
-    }
-
-    ReactS3.upload(fileData, config)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
   }
 };
