c715a49edcd756ba5e708fe98fe2fa35b6845e45
KDarch928 <Kayle.Drucker@gmail.com>
2018-05-12 19:48:23 -0500

    get app working again

---
+++ client/src/components/Goalheader/Goalheader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../style.css";
+
+
+const Goalheader = () => (
+  <div>
+    <h1 id="header">Your Goals</h1>
+    <br />
+ </div>
+);
+
+export default Goalheader;
